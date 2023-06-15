2f027a0180a4d51df5dd536055e36537b9487ba8
patricia mataga <patriciamataga@gmail.com>
2023-06-15 13:56:59 -0700

    Task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,11 @@
  * Return: 0 (Success)
  */
 int main(void)
-printf("Size of char: %d byte(s)", sizeof(char));
-printf("Size of int: %d byte(s)", sizeof(int));
-printf("Size of long int: %d byte(s)", sizeof(long int));
-printf("Size of long long int: %d byte(s)", sizeof(long long int));
-printf("Size of float: %d byte(s)", sizeof(float));
+{
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
