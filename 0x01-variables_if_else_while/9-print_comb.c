a1b5de4a2b81c523dd16d1b4bdc3a161b6607821
patricia mataga <patriciamataga@gmail.com>
2023-06-16 15:26:58 -0700

    Task 9 commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,24 +8,16 @@ int main(void)
 {
 	int num;
 
-for (num = 0; num <= 9; num++)
-{
-putchar(num + '0');
-putchar(',');
-putchar(' ');
-}
-
-for (num = 10; num <= 15; num++)
-{
-putchar(num - 10 + 'a');
-if (num != 15)
-{
-putchar(',');
-putchar(' ');
-}
-}
-
+	for (num = 48; num <= 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+	
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
-
 	return (0);
 }
