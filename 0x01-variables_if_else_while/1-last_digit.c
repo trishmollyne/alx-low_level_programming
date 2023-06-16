c702d472439aca551e3cf43f20fa0377293d023e
patricia mataga <patriciamataga@gmail.com>
2023-06-16 11:04:08 -0700

    Task 1 commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (a > 5)
 	{
-		printf("%d and is greater than 5\n", a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
 	else if (a == 0)
 	{
-		printf("%d and is 0\n", a);
+		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
 	else
 	{
-		printf("%d and is less than 6 and not 0\n", a);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 
 return (0);
