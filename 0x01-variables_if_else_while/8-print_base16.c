52397bb4c894a67f56950b5d98932af7fd7f699b
patricia mataga <patriciamataga@gmail.com>
2023-06-16 14:05:22 -0700

    Task 8 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - all your code should be in the main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	char num;
+	char pm;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar ((num % 10) + '0');
+
+	for (pm = 'a'; pm <= 'f'; pm++)
+		putchar(pm);
+
+	putchar('\n');
+
+	return (0);
+}
