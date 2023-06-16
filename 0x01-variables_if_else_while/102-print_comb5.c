cc947309bea23b3b0a500e962a416a463d9d01b8
patricia mataga <patriciamataga@gmail.com>
2023-06-16 16:34:08 -0700

    Task 12 commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - all your code should be in the main function
+ * Return: Always 0
+ */
+int main(void)
+{
+		int e, f;
+
+	for (e = 0; e <= 99; e++)
+	{
+		for (f = e; f <= 99; f++)
+		{
+			putchar((e / 10) + '0');
+			putchar((e % 10) + '0');
+			putchar(' ');
+			putchar((f / 10) + '0');
+			putchar((f % 10) + '0');
+
+			if (e != 99 || f != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
