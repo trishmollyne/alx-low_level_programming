e36c5cdb0ef27fdb1468b8dfd5ec73ce3b7c92a0
patricia mataga <patriciamataga@gmail.com>
2023-06-16 11:29:37 -0700

    Task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -all your code should be in the main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	char sl;
+
+	for (sl = 'a'; sl <= 'z'; sl++)
+	{
+		putchar(sl);
+	}
+	putchar('\n');
+
+return (0);
+}
