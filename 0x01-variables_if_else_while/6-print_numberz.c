0628b6753f3d7e64c2b37db565e475ac27205540
patricia mataga <patriciamataga@gmail.com>
2023-06-16 13:34:20 -0700

    Task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - you are not allowed to use any variable of type char
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
