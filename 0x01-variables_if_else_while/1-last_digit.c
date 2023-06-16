efd26a4567d846ca09d5a75bb21a574c1051c45d
patricia mataga <patriciamataga@gmail.com>
2023-06-16 10:41:42 -0700

    Task 1 commmit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - You will assign random number to the variable
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+
+	if (a > 5)
+	{
+		printf("%d and is greater than 5\n", a);
+	}
+	else if (a == 0)
+	{
+		printf("%d and is 0\n", a);
+	}
+	else
+	{
+		printf("%d and is less than 6 and not 0\n", a);
+	}
+
+return (0);
+}
