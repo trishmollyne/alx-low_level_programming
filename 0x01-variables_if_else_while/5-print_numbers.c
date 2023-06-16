9f9d725a3fdd4082da666c08fb9467dd0b3efc56
patricia mataga <patriciamataga@gmail.com>
2023-06-16 13:20:18 -0700

    Task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - all your code should be in the main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
