9eeafb8b364849a611cb1fb7a918be33927c68ed
patricia mataga <patriciamataga@gmail.com>
2023-06-20 12:10:38 -0700

    Life of Engineer P Mataga

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - you can only use putchar twice in your code
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char p;
+
+	for (p = 'w'; p <= 'y'; p++)
+	{_putchar(p);
+	}
+	_putchar('\n');
+}
