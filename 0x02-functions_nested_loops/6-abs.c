feb55976fb82e2771c61142b13c25213251d7342
patricia mataga <patriciamataga@gmail.com>
2023-06-20 16:02:41 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - i am supposed to write a function that computes interger value
+ * @n: Interger value 
+ * Return: value of n
+ */
+
+int _abs(int n)
+{
+	int _abs(int n)
+		return (n);
+	else 
+		return (-n;)
+}
