b642c26f60c3a1769233f6defdd31091d0e22601
patricia mataga <patriciamataga@gmail.com>
2023-06-20 22:11:41 -0700

    Number 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - two integers need to be added
+ * @p: firts integer
+ * @m: seconf interger
+ * Return: the value of the interger
+ */
+
+int add(int p, int m)
+{
+	int sum = p + m;
+	return sum;
+}
