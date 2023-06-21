89c480805548df2d46777946820809f4a95823b9
patricia mataga <patriciamataga@gmail.com>
2023-06-20 23:21:48 -0700

    Life of Engineer P Mataga

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print alphabet - prints the alphabet in lowercase followed by a new line
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
  * Return: void
  */
 
