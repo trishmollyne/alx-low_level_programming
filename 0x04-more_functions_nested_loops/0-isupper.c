6ff42c7535df3a9f9c6982f9291a3739244d3fba
patricia mataga <patriciamataga@gmail.com>
2023-06-22 10:18:13 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isupper - returns 1 if c is uppercase
+* @c: Variable text
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
