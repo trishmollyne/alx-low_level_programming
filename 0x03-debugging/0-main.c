a9170a6d09564c16a589cc7a1dd7c74a587691a1
patricia mataga <patriciamataga@gmail.com>
2023-06-22 06:37:19 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - functions for positive or negative to be tested
+* Return: 0
+*/
+
+int main(void)
+
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}
