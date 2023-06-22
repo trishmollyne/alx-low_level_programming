95d45dd652279d93c6bfb3d5f9b6e6623dc4ab08
patricia mataga <patriciamataga@gmail.com>
2023-06-22 10:52:13 -0700

    Engineer P Mataga codes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - you need t write a function that multiple intergers
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
