8e1cb151e93a3d20b3f9c5bfb7dca73fb6a57266
patricia mataga <patriciamataga@gmail.com>
2023-06-25 07:45:58 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,27 +1,49 @@
-#include "main.h"
-
+#iinclude "main.h"
 /**
  * print_times_table - when n is greater than 15 or less than 0 dont print
  * @n: how many times
  */
-
 void print_times_table(int n)
 {
-	int x, y, result;
-
-	if (n < 0 || n > 15)
-		return;
-
-	for (x = 0; x <= n; x++)
-	{
-		for (y = 0; y <= n; y++)
-		{
-			result = x * y;
-			if (y == 0)
-				printf("%d", result);
-			else
-				printf(", %d", result);
-		}
-		printf("\n");
-	}
+int w, x, y;
+if (n >= 0 && n <= 15)
+{
+for (w = 0; w <= n; w++)
+{
+for (x = 0; x <= n; x++)
+{
+y = x * w;
+if (x == 0)
+{
+_putchar(y + '0');
+}
+else if (y < 10 && x != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(y + '0');
+}
+else if (y >= 10 && y < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+}
+else if
+(y >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((y / 100) + '0');
+_putchar(((y / 10) % 10) + '0');
+_putchar((y % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
 }
