305d869211ee985a018037d66fdefaa8e0a4825d
patricia mataga <patriciamataga@gmail.com>
2023-06-26 11:03:46 -0700

    Engineer Mataga

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - parameter and updates the value points to 98
+ * @n: the interger input pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
