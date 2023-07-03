cbb4bca959b52a869c537d98d802febed91e2cc3
patricia mataga <patriciamataga@gmail.com>
2023-07-03 10:53:32 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int p;
+
+while (*s)
+{
+for (p = 0; accept[p]; p++)
+{
+if (*s == accept[p])
+return (s);
+}
+s++;
+}
+
+return ('\0');
+}
