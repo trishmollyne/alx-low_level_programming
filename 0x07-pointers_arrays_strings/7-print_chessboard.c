aba1d8b48cb81723dd2a9989e43b64b5085e5132
patricia mataga <patriciamataga@gmail.com>
2023-07-03 11:42:27 -0700

    Engineer T 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - a function that prints the chessboard
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+int y;
+int z;
+for (y = 0; y < 8; y++)
+{
+for (z = 0; z < 8; z++)
+_putchar(a[y][z]);
+_putchar('\n');
+}
+}
