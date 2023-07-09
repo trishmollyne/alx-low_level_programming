6940fce2d6055e45101228dd233731503b3b085a
patricia mataga <patriciamataga@gmail.com>
2023-07-09 04:45:20 -0700

    Trish 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,34 +2,48 @@
 #include "main.h"
 
 /**
- * main - Write a program that multiplies two numbers
- * @argc: The argument count
- * @argv: The argument vector
- * Return: 0 on success, 1 on error
- */
-int main(int argc, char *argv[])
-{
-int num1, num2, result;
-if (argc != 3)
-{
-printf("Error\n");
-return (1);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
+* _atoi - converts a string to an integer
+* @s: string to be converted
+* Return: the int converted from the string
+*/
 
-result = multiply(num1, num2);
-printf("%d\n", result);
-return (0);
-}
+int _atoi(char *s)
 
-/**
- * multiply - Multiply two numbers
- * @a: The first number
- * @b: The second number
- * Return: The result of the multiplication
- */
-int multiply(int a, int b)
 {
-return (a * b);
+
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
 }
