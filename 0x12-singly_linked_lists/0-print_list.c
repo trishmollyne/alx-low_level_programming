bf39576b2fe9cd6862f9dc88171ac3807cbc8e31
patricia mataga <patriciamataga@gmail.com>
2023-07-27 14:51:12 -0700

    Engineer T

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,16 +5,16 @@
  * list_len - returns the number of elements in a linked list
  * @h: pointer to the list_t list
  *
- * Return: The number of elements in h
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t m = 0;
+size_t m = 0;
 
-	while (h)
-	{
-		m++;
-		h = h->next;
-	}
-	return (m);
+while (h)
+{
+m++;
+h = h->next;
+}
+return (m);
 }
