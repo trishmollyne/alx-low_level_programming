a33ae0c79b89094726e5dc0c56a4979a22acd212
patricia mataga <patriciamataga@gmail.com>
2023-08-01 14:42:47 -0700

    TM

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,22 +1,17 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees linked list of integers and sets head pointer to NULL
+ * free_listint2 - Frees linked list and sets the head pointer to NULL
  * @head: Double pointer to the head of the linked list
  */
 void free_listint2(listint_t **head)
 {
-if
-(head == NULL)
-return;
-listint_t *current = *head;
-listint_t *next;
-while
-(current != NULL)
+if (head == NULL)
+while (*head)
 {
-next = current->next;
-free(current);
-current = next;
+temp = (*head)->next;
+free(*head);
+*head = temp;
 }
 *head = NULL;
 }
