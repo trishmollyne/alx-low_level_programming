f705e376d287da5982f7a7501bb042692ff60f61
patricia mataga <patriciamataga@gmail.com>
2023-08-02 09:48:38 -0700

    TM

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,15 +8,12 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-listint_t *current = head;
+listint_t *temp = head;
 unsigned int count = 0;
 while
-(current != NULL)
+(temp && count < NULL)
 {
-if
-(count == index)
-return (current);
-current = current->next;
+temp = temp->next;
 count++;
 }
 return (temp ? temp : NULL);
