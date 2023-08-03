821506c271362a93f311fba307945ca696ee7baa
patricia mataga <patriciamataga@gmail.com>
2023-08-03 13:59:03 -0700

    Trish

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,29 +1,16 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - Writes a character to the standard output (stdout).
- * @c: The character to be written.
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: The unsigned long int number to check.
+ * @index: The index of the bit to retrieve (starting from 0).
  *
- * Return: On success, the character written is returned.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: The value of the bit at the specified index or -1 if error occurred
  */
-int _putchar(char c)
+int get_bit(unsigned long int n, unsigned int index)
 {
-return (write(1, &c, 1));
-}
-
-/**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to print in binary representation.
- */
-void print_binary(unsigned long int n)
-{
-if (n > 1)
-print_binary(n >> 1);
-_putchar((n & 1) + '0');
-if (n == 1)
-return;
-if ((n & 1) == 0)
-_putchar('\n');
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
+unsigned long int mask = 1UL << index;
+return ((n & mask) ? 1 : 0);
 }
