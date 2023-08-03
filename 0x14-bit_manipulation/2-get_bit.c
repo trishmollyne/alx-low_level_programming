21f9784ff0b0d7769151b48fd12ab552ae28f4be
patricia mataga <patriciamataga@gmail.com>
2023-08-03 12:45:59 -0700

    TM

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,18 +2,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to the standard output (stdout).
- * @c: The character to be written.
- *
- * Return: On success, the character written is returned.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to print in binary representation.
  */
